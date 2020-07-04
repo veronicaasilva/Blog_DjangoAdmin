6aeaf81e637faeb66306c437336468bfb5209fc2
Verônica A Silva <veh.silva@gmail.com>
2020-07-04 11:25:37 -0300

    Criando os models e conf. Django Admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Post, Categoria
 
-# Register your models here.
+admin.site.site_header = "Post Admin"
+admin.site.site_title = "Administração Postagens"
+admin.site.index_title = 'Sistema de Gerenciamento de Posts'
+
+admin.site.register(Categoria)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('titulo', '_autor', 'categoria', 'data', 'status')
+    list_filter = ('autor','categoria', 'data', 'status')
+    exclude = ['autor']
+    search_fields = ['titulo', 'conteudo', 'categoria']
+    prepopulated_fields = {'slug': ('titulo',)}
+
+    def _autor(self, instance):
+        return f'{instance.autor.get_full_name()}'
+
+    def get_queryset(self, request):
+        qs = super(PostAdmin, self).get_queryset(request)
+        return qs.filter(autor=request.user)
+
+    def save_model(self, request, obj, form, change):
+        obj.autor = request.user
+        super().save_model(request, obj, form, change)
+
+admin.site.register(Post, PostAdmin)
+
+
+
+
+
+
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,56 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.urls import reverse 
+
+
+class Categoria(models.Model):
+    categoria = models.CharField(
+        max_length=100,
+        help_text='Insira uma categoria (Ex.: Drama)'
+    )
+
+    def __str__(self):
+        return self.categoria
+
+
+class Post(models.Model):
+    STATUS = (
+        (0,"Aguardando"),
+        (1,"Publicado")
+    )
+    autor = models.ForeignKey(
+        get_user_model(), 
+        on_delete=models.SET_NULL, 
+        null=True,
+        related_name='blog_posts'
+    )
+    categoria = models.ForeignKey(
+        Categoria, 
+        on_delete=models.SET_NULL,
+        null =  True
+    )
+    data = models.DateField(auto_now_add=True)
+    titulo = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    conteudo = models.TextField()
+    status = models.IntegerField(choices=STATUS, default=0)
+
+
+    class Meta:
+        ordering = ['-data']
+
+    '''def display_categoria(self):
+        return ', '.join([categoria.categoria for categoria in self.categoria.all()[:3]])
+
+    display_categoria.short_description = 'Categoria(s)' '''
+
+    '''
+    def get_absolute_url(self):
+        return reverse('post-detail', args=[str(self.id)])'''
+
+    def __str__(self):
+        return self.titulo
+
+
